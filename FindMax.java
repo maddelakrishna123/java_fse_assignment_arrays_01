b983e70cb57f9ceb9c3761ea1475588e1d509282
Krshna Kumar <maddelakrishnakumar@gmail.com>
2023-06-22 19:24:04 +0530

    assignment question added

---
+++ FindMax.java
@@ -0,0 +1,16 @@
+/**
+    Write  the java program  to find the max of the list : 45 , 56,23, 23,80,13,20
+*/
+
+public class FindMax{
+
+
+public static void main(String[] args){
+
+
+// write the code here 
+
+
+}
+
+}
